d9a083697a54ef3beb2f13ff82417d5ad7afee24
Stefan Partin <stefanjp@gmail.com>
2019-02-24 14:13:07 -0800

    added csrf token

--- grader/main.py
+++ grader/main.py
@@ -1,5 +1,6 @@
 import os
 import base64
+import random
 
 from flask import Flask, request
 from model import Grade 
@@ -9,14 +10,20 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def home():
 
+    # If the session does not include a CSRF token, then add one.
+    if 'csrf_token' not in session:
+        session['csrf_token'] = str(random.randint(10000000, 99999999))
+
     if request.method == 'POST':
-        g = Grade(
-            student=request.form['student'],
-            assignment=request.form['assignment'],
-            grade=request.form['grade'],
-        )
-        #print("(" + request.form['grade'] + ")")
-        g.save()
+        # Only save the grade if the form submission includes a CSRF token,
+        # and it matches the token in the session.
+        if request.form.get('csrf_token', None) == session['csrf_token']:
+            g = Grade(
+                student=request.form['student'],
+                assignment=request.form['assignment'],
+                grade=request.form['grade'],
+                )
+            g.save()
 
     body = """
 <html>
@@ -34,12 +41,14 @@ def home():
     <label for="grade">Grade</label>
     <input type="text" name="grade"><br>
 
+    <input type="hidden" name="csrf_token" value="{}">   <!-- Include the CSRF token in the form -->
+
     <input type="submit" value="Submit">
 </form>
 
 <h2>Existing Grades</h2>
-"""
-    
+""".format(session['csrf_token'])   # <--
+
     for g in Grade.select():
         body += """
 <div class="grade">
@@ -47,7 +56,7 @@ def home():
 </div>
 """.format(g.student, g.assignment, g.grade)
 
-    return body 
+    return body
 
 
 if __name__ == "__main__":
